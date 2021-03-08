13aaa1c349d547b68b4f135911d14ac9f67be390
elx7 <77843884+elx7@users.noreply.github.com>
2021-03-08 10:43:08 +0100

    Reformatting class

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -11,70 +11,67 @@ import java.util.Set;
  */
 
 public class Customer {
-	private String name;
-	private static Set<Item> itemsOrdered;
-	private double totalOrderAmount;
-	private int totalNumberItems;
+    private String name;
+    private Set<Item> itemsOrdered;
+    private double totalOrderAmount;
+    private int totalNumberItems;
 	
     /**
      * Creates an Customer object.
      * @param nameIn the name of the customer
      */
-	public Customer(String nameIn) {
-		name = nameIn;
-		itemsOrdered = new HashSet<Item>();
-		setTotalOrderAmount();
-		setTotalNumberItems();
-	}
+    public Customer(String nameIn) {
+    	name = nameIn;
+	itemsOrdered = new HashSet<Item>();
+	setTotalOrderAmount();
+	setTotalNumberItems();
+    }
 	
-	public String getName()		    { return name; 	       }
-	public Set<Item> getItemsOrdered()  { return itemsOrdered;     }
-	public double getTotalOrderAmount() { return totalOrderAmount; }
-	public double getTotalNumberItems() { return totalNumberItems; }
+    public String getName()		{ return name; 	           }
+    public Set<Item> getItemsOrdered()  { return itemsOrdered;     }
+    public double getTotalOrderAmount() { return totalOrderAmount; }
+    public double getTotalNumberItems() { return totalNumberItems; }
 	
-	public void setName(String nameIn) {
-    	if (nameIn.trim().length() == 0) {
-        	throw new IllegalStateException("Customer name blank.");
-        }
+    public void setName(String nameIn) {
+    	if (nameIn.trim().length() == 0)
+       	    throw new IllegalStateException("Customer name blank.");
     	name = nameIn;
     }
 	
-	public void setItemsOrdered(Set<Item> itemsOrderedIn) {
-		itemsOrdered = itemsOrderedIn;
-	}
+    public void setItemsOrdered(Set<Item> itemsOrderedIn) {
+	itemsOrdered = itemsOrderedIn;
+    }
 
-	public void setTotalOrderAmount() {
-		double total = 0;
-		for (Item io : itemsOrdered) {
-			total += io.getPriceTotal();
-		}
-		//a call to the discounts methods on the total could be done here Shariq
-		totalOrderAmount = total;
-	}
+    public void setTotalOrderAmount() {
+	double total = 0;
+	for (Item io : itemsOrdered)
+	    total += io.getPriceTotal();
+	//a call to the discounts methods on the total could be done here Shariq
+	totalOrderAmount = total;
+    }
 	
-	public void setTotalNumberItems() {
-		int total = 0;
-		for (Item io : itemsOrdered) {
-			total += io.getQuantity();
-		}
-		totalNumberItems = total;
-	}
+    public void setTotalNumberItems() {
+	int total = 0;
+	for (Item io : itemsOrdered)
+	    total += io.getQuantity();
+	totalNumberItems = total;
+    }
 
     /**
      * Adds an Item to the list of items ordered by the customer.
      * @param item the item to add to the list
      */
-	public void addToItemsOrdered(Item item) {
-		for (Item io : itemsOrdered) {
-	        if (item.hashCode() == io.hashCode()) {
-	        	System.out.println(io.getName() + " already present in the customer's order.");
-	        	return;
-	        }
+    public void addToItemsOrdered(Item item) {
+        for (Item io : itemsOrdered) {
+	    if (item.hashCode() == io.hashCode()) {
+	        System.out.println(io.getName() + " already present in the customer's order.");
+	        return;
 	    }
-		itemsOrdered.add(item);
-	    System.out.println("-> " + item.getName() + ", " + item.getQuantity() + ", at " + item.getPrice() + " $ each for a total of " + item.getPriceTotal() + "$, added to " + getName() + "'s order.");
-	    return;
 	}
+	itemsOrdered.add(item);
+	System.out.println("-> " + item.getName() + ", " + item.getQuantity() + ", at " + item.getPrice() + " $ each for a total of " + item.getPriceTotal() + "$, added to " + getName() + "'s order.");
+	return;
+    }
 	
     /**
      * Necessary method to use a Customer in a HashSet
@@ -82,7 +79,7 @@ public class Customer {
      * their name being the element of comparison
      * @param other the Customer object to compare to
      */
-	public int compareTo(Customer other) {
+    public int compareTo(Customer other) {
     	return name.compareTo(other.name);
     }
     
