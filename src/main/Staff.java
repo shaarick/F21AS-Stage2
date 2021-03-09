94b57ad63df52a976277b1989aa31148134804a4
elx7 <77843884+elx7@users.noreply.github.com>
2021-03-09 18:42:54 +0100

    Correcting the thread's code
    Determining the thread's end's conditions,
    Adding support for any sleeping duration
    in coordination with the producer's thread and the queue

--- src/main/Staff.java
+++ src/main/Staff.java
@@ -1,28 +1,33 @@
 package main;
 
+/**
+ * Staff class
+ * This class manages the threads initialized by the different staff members
+ * that consume the customers's orders from the queue.
+ * @author Shariq FAROOQUI - msf2000 - H00358466
+ * + Nicolas JEAN - nj2000 - H00359359 for the run() method
+ */
+
 public class Staff implements Runnable {
-//	private Set<Customer> customerList;
 	private Queue<Customer> queue;
-	
+
 	public Staff(Queue<Customer> q) {
 		queue = q;
 	}
 
-	@Override
 	public void run() {
-		while(!queue.isEmpty() && !queue.isTerminated()) {
+		while (!queue.isTerminated()) {
 			try {
 				Customer test = queue.dequeue();
-				System.out.println(Thread.currentThread().getName() + " is serving " + test.getName());
-				System.out.println(queue.isEmpty());
+				if (!(test == null))
+					System.out.println(Thread.currentThread().getName() + " is serving " + test.getName());
 			} catch (EmptyQueueException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			try {Thread.currentThread().sleep(100);}
+			try { Thread.currentThread().sleep(1000); }
 			catch (InterruptedException e) {}
 		}
+		System.out.println(Thread.currentThread().getName() + " is done working.");
+		return;
 	}
 }
-
-//if producer is finished and queue empty ->end program
