4ec29bcce2262286277768058bcda8aea1e9a69d
wam4 <andrew_manson@aol.com>
2021-03-30 12:32:28 +0100

    Queue Test

--- src/test/QueueTest.java
+++ src/test/QueueTest.java
@@ -17,139 +17,51 @@ public class QueueTest {
 	public void test() {
 		assertEquals(0,queue.size());
 		assertTrue(queue.isEmpty());
-		try {
-			queue.dequeue();
-			fail();
-		}
-		catch(EmptyQueueException exception) {
-			assertTrue(exception.getMessage().equals("Queue is empty"));
-		}
-		try {
-			queue.front();
-			fail();
-		}
-		catch(EmptyQueueException exception) {
-			assertTrue(exception.getMessage().equals("Queue is empty"));
-		}
-		
-		try {
-			queue.enqueue("Andrew");
-			assertFalse(queue.isEmpty());
-			assertEquals(1,queue.size());
-			assertTrue(queue.front().equals("Andrew"));
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		try {
-			queue.enqueue("Shariq");
-			assertFalse(queue.isEmpty());
-			assertEquals(2,queue.size());
-			assertTrue(queue.front().equals("Andrew"));
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		
-		try {
-			assertTrue(queue.dequeue().equals("Andrew"));
-			assertFalse(queue.isEmpty());
-			assertEquals(1,queue.size());
-			assertTrue(queue.front().equals("Shariq"));
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		try {
-			queue.enqueue("Nicolas");
-			assertFalse(queue.isEmpty());
-			assertEquals(2,queue.size());
-			assertTrue(queue.front().equals("Shariq"));
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		
-		try {
-			assertTrue(queue.dequeue().equals("Shariq"));
-			assertFalse(queue.isEmpty());
-			assertEquals(1,queue.size());
-			assertTrue(queue.front().equals("Nicolas"));
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		
-		try {
-			assertTrue(queue.dequeue().equals("Nicolas"));
-			assertEquals(0,queue.size());
-			assertTrue(queue.isEmpty());
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		try {
-			queue.dequeue();
-			fail();
-		}
-		catch(EmptyQueueException exception) {
-			assertTrue(exception.getMessage().equals("Queue is empty"));
-		}
-		try {
-			queue.front();
-			fail();
-		}
-		catch(EmptyQueueException exception) {
-			assertTrue(exception.getMessage().equals("Queue is empty"));
-		}
+		//assertNull(queue.dequeue());
+		assertNull(queue.front());
 
-		try {
-			queue.enqueue("Rashid");
-			assertFalse(queue.isEmpty());
-			assertEquals(1,queue.size());
-			assertTrue(queue.front().equals("Rashid"));
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
+		queue.enqueue("Andrew");
+		assertFalse(queue.isEmpty());
+		assertEquals(1,queue.size());
+		assertTrue(queue.front().equals("Andrew"));
+
+		queue.enqueue("Shariq");
+		assertFalse(queue.isEmpty());
+		assertEquals(2,queue.size());
+		assertTrue(queue.front().equals("Andrew"));
 		
-		try {
-			assertTrue(queue.dequeue().equals("Rashid"));
-			assertEquals(0,queue.size());
-			assertTrue(queue.isEmpty());
-		}
-		catch(EmptyQueueException exception) {
-			fail();
-		}
-		try {
-			queue.dequeue();
-			fail();
-		}
-		catch(EmptyQueueException exception) {
-			assertTrue(exception.getMessage().equals("Queue is empty"));
-		}
-		try {
-			queue.front();
-			fail();
-		}
-		catch(EmptyQueueException exception) {
-			assertTrue(exception.getMessage().equals("Queue is empty"));
-		}
-	}
-	
-	
-	@Test(expected = EmptyQueueException.class)
-	public void dequeueException() throws EmptyQueueException {
+		assertTrue(queue.dequeue().equals("Andrew"));
+		assertFalse(queue.isEmpty());
+		assertEquals(1,queue.size());
+		assertTrue(queue.front().equals("Shariq"));
+		
+		queue.enqueue("Nicolas");
+		assertFalse(queue.isEmpty());
+		assertEquals(2,queue.size());
+		assertTrue(queue.front().equals("Shariq"));
+		
+		assertTrue(queue.dequeue().equals("Shariq"));
+		assertFalse(queue.isEmpty());
+		assertEquals(1,queue.size());
+		assertTrue(queue.front().equals("Nicolas"));
+		
+		assertTrue(queue.dequeue().equals("Nicolas"));
 		assertEquals(0,queue.size());
 		assertTrue(queue.isEmpty());
-		queue.dequeue();
-	}
-	
-	
-	@Test(expected = EmptyQueueException.class)
-	public void frontException() throws EmptyQueueException {
+		
+		//assertNull(queue.dequeue());
+		assertNull(queue.front());
+		
+		queue.enqueue("Rashid");
+		assertFalse(queue.isEmpty());
+		assertEquals(1,queue.size());
+		assertTrue(queue.front().equals("Rashid"));
+		
+		assertTrue(queue.dequeue().equals("Rashid"));
 		assertEquals(0,queue.size());
 		assertTrue(queue.isEmpty());
-		queue.front();
+		//assertNull(queue.dequeue());
+		assertNull(queue.front());
+		
 	}
 }
